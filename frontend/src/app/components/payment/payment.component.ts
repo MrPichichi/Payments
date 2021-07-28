58c9b2ea3ecdd698af5fd6df33ef750ae1115111
MrPichichi <franciscogamboa21@gmail.com>
2021-07-27 20:26:36 -0400

    finalizado

--- frontend/src/app/components/payment/payment.component.ts
+++ frontend/src/app/components/payment/payment.component.ts
@@ -126,8 +126,6 @@ export class PaymentComponent implements OnInit {
   resetForm(form?: NgForm) {
     if (form && editando == false){
       form.reset();
-      window.alert("Formulario reseteado");
-
     }
   }
   //validacion de la estructura de un formulario
